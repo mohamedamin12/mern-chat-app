81635c23b35bcc902d97ec779c9320dc96b64f13
mohamedamin12 <aboamin909080@gmail.com>
2025-02-11 18:30:01 +0200

    add getUsersForSidebar endpoint and protect route middleware

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,9 @@
+const { getUsersForSidebar } = require('../controllers/user.controller');
+const protectRoute = require('../middleware/protectRoute');
+
 const router = require('express').Router();
 
+router.get("/", protectRoute, getUsersForSidebar);
 
 
 
