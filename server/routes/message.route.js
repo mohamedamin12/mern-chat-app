83120691f70f7f5de7fa93fe830bd77329df4cbc
mohamedamin12 <aboamin909080@gmail.com>
2025-02-11 17:46:53 +0200

    implement messaging functionality with send and retrieve messages endpoints, add conversation model and protect routes middleware

--- server/routes/message.route.js
+++ server/routes/message.route.js
@@ -1,5 +1,10 @@
+const { sendMessage ,  getMessages} = require('../controllers/message.controller');
+const protectRoute = require('../middleware/protectRoute');
+
 const router = require('express').Router();
 
+router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages);
 
 
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser")
 const connectToDB = require("./db/connectToDB.js");
 const authRoutes = require("./routes/auth.route.js");
 const userRoutes = require("./routes/user.route.js");
-const messageRoutes = require("./routes/user.route.js");
+const messageRoutes = require("./routes/message.route.js");
 
 
 dotenv.config();
@@ -13,6 +14,9 @@ const PORT = process.env.PORT || 5555;
 
 //* Middleware to parse JSON request bodies
 app.use(express.json());
+
+//* Middleware to parse cookies
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
