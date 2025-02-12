23f73eddedaec0e727151e4552e77b964ec05b5c
mohamedamin12 <aboamin909080@gmail.com>
2025-02-12 13:21:24 +0200

    add Tailwind CSS configuration, implement sidebar and messaging components, and set up main application structure

---
+++ client/src/components/messages/Message.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>
+      <div className='chat-image avatar'>
+        <div className='w-10 rounded-full'>
+          <img alt='Tailwind CSS chat bubble component' />
+        </div>
+      </div>
+      <div className={`chat-bubble text-white  pb-2`}>message</div>
+      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>footer</div>
+    </div>
+  )
+}
+
+export default Message
---
+++ client/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto'>
+      <button className='btn btn-sm btn-outline-danger'>
+        Logout
+      </button>
+    </div>
+  )
+}
+
+export default LogoutButton
